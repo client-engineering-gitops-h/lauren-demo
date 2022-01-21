00ab6d278820b8bc1be69db146e03b5650bfc91d
Andrew Jyan <andrew.jyan@ibm.com>
2022-01-20 20:01:52 -0500

    restyled card container

--- server/server.js
+++ server/server.js
@@ -7,16 +7,12 @@ const port = process.env.PORT || "3000";
 const delay = 1000 * (process.env.DELAY || 0);
 
 server.listen(port, function () {
-    console.log(`Server listening on http://localhost:${port}`);
+  console.log(`Server listening on http://localhost:${port}`);
 });
 
-
-
-
 setTimeout(() => {
-    app.get('/hello', (req, res) => res.send('Hello World Node.js Wow so fast'));
-    app.get('/ready', (req, res) => res.json({ status: 'UP' }));
-    app.use(express.static(path.join(__dirname, "../build")));
-    console.log(`app is ready for ${process.env.NODE_ENV}`);
+  app.get("/hello", (req, res) => res.send("Hello World Node.js Wow so fast"));
+  app.get("/ready", (req, res) => res.json({ status: "UP" }));
+  app.use(express.static(path.join(__dirname, "../build")));
+  console.log(`app is ready for ${process.env.NODE_ENV}`);
 }, delay);
-
