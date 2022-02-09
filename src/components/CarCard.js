bb4c5c61b7d8f7db9b537c7eef77f2bc983a5de1
Andrew Jyan <andrew.jyan@ibm.com>
2022-02-09 15:00:43 -0500

    created state for make and model

--- src/components/CarCard.js
+++ src/components/CarCard.js
@@ -6,53 +6,72 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 
 const CarCard = ({ setSelectedCar, setCarCoordinates }) => {
   const [cars, setCars] = useState({
-      "1G1FZ6S02M4108532": {
-        vid: "1G1FZ6S02M4108532",
-        imei: "12310293819",
-        make: null,
-        model: null,
-      },
+    "1G1FZ6S02M4108532": {
+      vid: "1G1FZ6S02M4108532",
+      imei: "12310293819",
+      make: null,
+      model: null,
+    },
 
-      "WBXPA93415WD09324": {
+    WBXPA93415WD09324: {
       vid: "WBXPA93415WD09324",
       imei: "12310",
-      make: null, 
-      model: null },
-      
+      make: null,
+      model: null,
+    },
 
-      "1J8HH48P17C686412" : {
+    "1J8HH48P17C686412": {
       vid: "1J8HH48P17C686412",
       imei: "12131",
       make: null,
-      model: null
-},
+      model: null,
+    },
   });
-const [mileage, setMileage] = useState({
+  const [mileage, setMileage] = useState({
     "1G1FZ6S02M4108532": {
-        vid: "1G1FZ6S02M4108532",
-        epm: "12310293819",
-        latitude: 26.402623,
-        longitude: -81.80928,
-        tracker_mileage: 1231
-  },
-  
-  "WBXPA93415WD09324": {
+      vid: "1G1FZ6S02M4108532",
+      epm: "12310293819",
+      latitude: 26.402623,
+      longitude: -81.80928,
+      tracker_mileage: 1231,
+    },
+
+    WBXPA93415WD09324: {
       vid: "WBXPA93415WD09324",
       epm: "12310293819",
-        latitude: 26.41103,
-        longitude: -81.812311,
-        tracker_mileage: 1231
-  },
-  
-  "1J8HH48P17C686412": {
-    vid: "1J8HH48P17C686412",
-    epm: "12310293819",
-    latitude: 26.41126,
-    longitude: -81.8081235,
-    tracker_mileage: 1231
-  },
-  
- });
+      latitude: 26.41103,
+      longitude: -81.812311,
+      tracker_mileage: 1231,
+    },
+
+    "1J8HH48P17C686412": {
+      vid: "1J8HH48P17C686412",
+      epm: "12310293819",
+      latitude: 26.41126,
+      longitude: -81.8081235,
+      tracker_mileage: 1231,
+    },
+  });
+  const [makeModel, setMakeModel] = useState({
+    "1G1FZ6S02M4108532": {
+      make: "",
+      model: "",
+      year: "",
+    },
+
+    WBXPA93415WD09324: {
+      make: "",
+      model: "",
+      year: "",
+    },
+
+    "1J8HH48P17C686412": {
+      make: "",
+      model: "",
+      year: "",
+    },
+  });
+
   const [counter, setCounter] = useState(0);
 
   // useEffect(() => {
@@ -96,34 +115,35 @@ const [mileage, setMileage] = useState({
   }, [counter]);
 
   useEffect(() => {
-    for (const key in cars) {
-      if (!(cars[key].make || cars[key].model || cars[key].year)) {
-        axios
-          .get(
-            `https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVin/${key}?format=json`
-          )
-          .then(({ data }) => {
-            const make = data.Results[6].Value;
-            const model = data.Results[8].Value;
-            const year = data.Results[9].Value;
-            console.log("data from api", data, "make", make, "year", year);
-            const updatedMakeModel = { make: make, model: model, year: year };
-            setCars({ ...cars, [key]: { ...cars[key], ...updatedMakeModel } });
-          });
-      }
+    for (const key in makeModel) {
+      axios
+        .get(
+          `https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVin/${key}?format=json`
+        )
+        .then(({ data }) => {
+          const make = data.Results[6].Value;
+          const model = data.Results[8].Value;
+          const year = data.Results[9].Value;
+          const updatedMakeModel = {
+            [key]: { make: make, model: model, year: year },
+          };
+          setMakeModel({ ...makeModel, ...updatedMakeModel });
+        });
     }
   }, []);
 
-  console.log("mileage", mileage)
+  console.log("mileage", mileage);
 
   useEffect(() => {
     setCarCoordinates(mileage);
   }, [mileage]);
 
+  console.log("makeModel", makeModel);
   return (
     <div>
       {cars &&
         mileage &&
+        makeModel &&
         Object.keys(cars).map((key, i) => {
           const car = cars[key];
           const carMileage = mileage[key];
@@ -134,7 +154,12 @@ const [mileage, setMileage] = useState({
               interactive={true}
               elevation={Elevation.One}
             >
-            <CollapseContent  setSelectedCar={setSelectedCar} car={car} carMileage={carMileage}/>
+              <CollapseContent
+                setSelectedCar={setSelectedCar}
+                car={car}
+                carMileage={carMileage}
+                makeModel={makeModel}
+              />
             </Card>
           );
         })}
--- src/components/CollapseContent.js
+++ src/components/CollapseContent.js
@@ -1,84 +1,84 @@
 import React, { useState, useEffect } from "react";
 import { Collapse, Button, Icon } from "@blueprintjs/core";
 
-const CollapseContent = ({car, setSelectedCar, carMileage }) => {
+const CollapseContent = ({ car, setSelectedCar, carMileage, makeModel }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+  console.log("car", car);
+  console.log(makeModel, "make and model in collapse");
+  const vid = car.vid;
 
-  console.log("check car data", car)
-  console.log("car mileage in content", carMileage)
-const [isOpen, setIsOpen] = useState(false);
-const handleClick = () => {
-  setIsOpen(!isOpen);
+  return (
+    <div>
+      {car && carMileage && makeModel[vid] && (
+        <>
+          <Button
+            className="collapse-card-button"
+            minimal={true}
+            onClick={() => {
+              handleClick();
+              setSelectedCar(car.vid);
+            }}
+          >
+            <div className="collapse-card">
+              <div className="card-title-vin">
+                <Icon size={30} icon="drive-time" />
+                <h2 style={{ paddingLeft: "1rem" }}>VIN: {car.vid}</h2>
+              </div>
+              <div className="icon-collapse-card">
+                <Icon size={20} icon="chevron-down" />
+              </div>
+            </div>
+          </Button>
+          <Collapse isOpen={isOpen}>
+            <div className="customer-details">
+              <div>
+                <strong>IMEI: </strong>
+                {car.imei || "N/A"}
+              </div>
+              <div>
+                <strong>Customer: </strong>IBM
+              </div>
+              <div>
+                <strong>Onboarded: </strong>
+                {new Date(car.created_at).toLocaleString() || "N/A"}
+              </div>
+              <div>
+                <strong>Last Active: </strong>
+                {new Date(car.updated_at).toLocaleString() || "N/A"}
+              </div>
+              <div style={{ paddingTop: "10px" }}>
+                <strong>Make: </strong>
+                {makeModel[vid].make || "N/A"}
+              </div>
+              <div>
+                <strong>Model: </strong>
+                {makeModel[vid].model || "N/A"}
+              </div>
+              <strong>Year: </strong>
+              {makeModel[vid].year || "N/A"}
+            </div>
+            <div className="location-details">
+              <div>
+                <strong>Mileage: </strong>
+                {carMileage.tracker_mileage || "N/A"} mi
+              </div>
+              <div>
+                <strong>Lat: </strong>
+                {carMileage.latitude.toFixed(3) || "N/A"}
+              </div>
+              <div>
+                <strong>Long: </strong>
+                {carMileage.longitude.toFixed(3) || "N/A"}
+              </div>
+            </div>
+          </Collapse>
+        </>
+      )}
+    </div>
+  );
 };
 
-return(
-  <div>
-    { car &&
-      carMileage &&
-    <>
-    <Button
-      className="collapse-card-button"
-      minimal={true}
-      onClick={() => {
-        handleClick();
-        setSelectedCar(car.vid);
-      }}
-    >
-      <div className="collapse-card">
-        <div className="card-title-vin">
-          <Icon size={30} icon="drive-time" />
-          <h2 style={{ paddingLeft: "1rem" }}>VIN: {car.vid}</h2>
-        </div>
-        <div className="icon-collapse-card">
-          <Icon size={20} icon="chevron-down" />
-        </div>
-      </div>
-    </Button>
-  <Collapse isOpen={isOpen}>
-    <div className="customer-details">
-      <div>
-        <strong>IMEI: </strong>
-        {car.imei || "N/A"}
-      </div>
-      <div>
-        <strong>Customer: </strong>IBM
-      </div>
-      <div>
-        <strong>Onboarded: </strong>
-        {new Date(car.created_at).toLocaleString() || "N/A"}
-      </div>
-      <div>
-        <strong>Last Active: </strong>
-        {new Date(car.updated_at).toLocaleString() || "N/A"}
-      </div>
-      <div style={{ paddingTop: "10px" }}>
-        <strong>Make: </strong>
-        {car.make || "N/A"}
-      </div>
-      <div>
-        <strong>Model: </strong>
-        {car.model || "N/A"}
-      </div>
-      <strong>Year: </strong>
-      {car.year || "N/A"}
-    </div>
-    <div className="location-details">
-      <div>
-        <strong>Mileage: </strong>
-        {carMileage.tracker_mileage || "N/A"} mi
-      </div>
-      <div>
-        <strong>Lat: </strong>
-        {carMileage.latitude.toFixed(3) || "N/A"}
-      </div>
-      <div>
-        <strong>Long: </strong>
-        {carMileage.longitude.toFixed(3) || "N/A"}
-      </div>
-      </div>
-    </Collapse>
-  </>
-  }
-  </div>
-)}
-
-export default CollapseContent
+export default CollapseContent;
