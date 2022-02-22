4d2b90e98fc27b4842ffe81cb7107766c777f410
Andrew Jyan <andrew.jyan@ibm.com>
2022-02-22 16:19:38 -0500

    very real car movement

--- src/components/HertzMap.js
+++ src/components/HertzMap.js
@@ -1,8 +1,66 @@
-import React, { useRef } from "react";
+import { arrayLengthCompare } from "@blueprintjs/core/lib/esm/common/utils";
+import React, { useState, useEffect } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
 import "../styles/DigitalMesh.css";
 
+const coordinates = [
+  {
+    lat: 26.970733,
+    lng: -82.305153,
+  },
+  {
+    lat: 26.970743,
+    lng: -82.305239,
+  },
+  {
+    lat: 26.970733,
+    lng: -82.305314,
+  },
+  {
+    lat: 26.970781,
+    lng: -82.305432,
+  },
+  {
+    lat: 26.97081,
+    lng: -82.30555,
+  },
+  {
+    lat: 26.970834,
+    lng: -82.305636,
+  },
+  {
+    lat: 26.970863,
+    lng: -82.305716,
+  },
+  {
+    lat: 26.970891,
+    lng: -82.305818,
+  },
+  {
+    lat: 26.970939,
+    lng: -82.305915,
+  },
+];
+
+const sleep = (milliseconds) => {
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+};
+
 function HertzMap({ selectedCarMarkers }) {
+  const [currPosition, setCurrPosition] = useState(coordinates[0]);
+
+  const moveCar = async () => {
+    for (let i = 0; i < coordinates.length; i++) {
+      await sleep(5000);
+      setCurrPosition(coordinates[i]);
+    }
+  };
+
+  useEffect(() => {
+    moveCar();
+  }, []);
+
   return (
     <div>
       <MapContainer center={[26.971, -82.305]} zoom={10}>
@@ -11,23 +69,12 @@ function HertzMap({ selectedCarMarkers }) {
           url="https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=09kOwvflRhlbJpyDLsaQ"
         />
 
-        {selectedCarMarkers &&
-          selectedCarMarkers.map((car, i) => {
-            const carLatLong = {
-              lat: car.latitude,
-              lng: car.longitude,
-            };
-
-            return (
-              <Marker key={i} position={carLatLong}>
-                <Popup>
-                  VIN: {car.vid}
-                  <br />
-                  Last Updated: {new Date().toLocaleTimeString("en-US")}
-                </Popup>
-              </Marker>
-            );
-          })}
+        <Marker position={currPosition}>
+          <Popup>
+            <br />
+            Last Updated: {new Date().toLocaleTimeString("en-US")}
+          </Popup>
+        </Marker>
       </MapContainer>
     </div>
   );
